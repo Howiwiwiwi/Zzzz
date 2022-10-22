dd6ee3fe0d1802105d0f1116a96c5bcccc78a57e
Howiwiwiwi <115839750+Howiwiwiwi@users.noreply.github.com>
2022-10-22 22:14:27 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,21 @@
+from pyrogram import Client, filters
+
+bot = Client(
+
+"my first project in pyrogram",
+
+ api_id = 9028013,
+
+ api_hash = "cc894fc40424f9c8bbcf06b7355bd69d",
+
+ bot_token = "5334586594:AAHN34STvQnny1CV1v9y-9a701olsgizFmo"
+
+)
+
+@bot.on_message(filters.command("start") & filters.private)
+
+def start(bot, message):
+
+ bot.send_message(message.chat.id , "hello")
+
+bot.run()
